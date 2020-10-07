7d2d13ba82b53ec591fe1f65bc264d730c2a1499
dannynurdin <dannyfachrul@gmail.com>
2020-10-07 14:03:48 +0700

    add more layout

--- image/views.py
+++ image/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render 
+from .models import Employee
   
 def index_view(request): 
     return render(request, "index.html")
 
 def employee_view(request):
+    employees = Employee.objects.all
 
-
-    return render(request, "pekerja.html")  
+    return render(request, "pekerja.html", {'employees':employees})  
